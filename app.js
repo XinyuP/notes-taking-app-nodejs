28645ef078a58f80985bca9339ba5b32aa91bee0
XinyuP <xinyup@umich.edu>
2022-12-12 19:57:56 -0500

    getNotes function

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const add = require('./utils.js')
-const sum = add(1, 3)
-console.log(sum)
+const getNotes = require('./notes.js')
+const msg = getNotes()
+console.log(msg)
+
+
+
+
+
+
+
+
+// const add = require('./utils.js')
+// const sum = add(1, 3)
+// console.log(sum)
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNodes = function() {
+    return "Your notes...."
+}
+
+module.exports = getNodes
