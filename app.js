ea5374c511b6f8e0dbb0ef8657654f87920995c6
XinyuP <xinyup@umich.edu>
2022-12-12 20:12:28 -0500

    npm modules

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+var validator = require('validator');
 const getNotes = require('./notes.js')
 const msg = getNotes()
 console.log(msg)
 
 
-
+console.log(validator.isEmail('11@qq.com'))
+console.log(validator.isURL('https://qq.com'))
+console.log(validator.isURL('https:/.com'))
 
 
 
