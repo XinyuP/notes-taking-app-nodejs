72d7261c5fa16d7cf2eb32af6dadb5cf7571ae16
XinyuP <xinyup@umich.edu>
2022-12-12 21:42:14 -0500

    add remove read list yargs command

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ const yargs = require('yargs')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note!!!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 
 })
@@ -46,7 +59,8 @@ yargs.command({
 })
 
 //console.log(process.argv)
-console.log(yargs.argv)
+yargs.parse()
+//console.log(yargs.argv)
 
 
 
