cde49c219fb3015368475e52b81b391bb7bb6473
XinyuP <xinyup@umich.edu>
2022-12-12 21:17:15 -0500

    process.argv, command line input

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
 var validator = require('validator');
-//import chalk from 'chalk';
 const chalk = require('chalk');
-const getNotes = require('./notes.js')
-const msg = getNotes()
-console.log(msg)
 
 
-console.log(validator.isEmail('11@qq.com'))
-console.log(validator.isURL('https://qq.com'))
-// console.log(validator.isURL('https:/.com'))
+const command = process.argv[2]
+console.log(process.argv)
 
 
-console.log(chalk.bold.green.inverse('Success!'));
-console.log(chalk.inverse.red.bold('fail!'));
-console.log(chalk.bold.yellow.underline.inverse('yeah'));
+if (command === 'add') {
+    console.log('Adding note!')
+}
+else if (command === 'remove') {
+    console.log('Removing note!')
+}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getNotes = require('./notes.js')
+// const msg = getNotes()
+// console.log(msg)
+
+
+// console.log(validator.isEmail('11@qq.com'))
+// console.log(validator.isURL('https://qq.com'))
+// // console.log(validator.isURL('https:/.com'))
+
+
+// console.log(chalk.bold.green.inverse('Success!'));
+// console.log(chalk.inverse.red.bold('fail!'));
+// console.log(chalk.bold.yellow.underline.inverse('yeah'));
+
+
+
+// console.log(process.argv)
 
 
 // const add = require('./utils.js')
