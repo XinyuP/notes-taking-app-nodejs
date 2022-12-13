5440f08cf9506979084e0e3b7c0e82300a9f5eed
XinyuP <xinyup@umich.edu>
2022-12-12 20:36:07 -0500

    chalk string styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var validator = require('validator');
+//import chalk from 'chalk';
+const chalk = require('chalk');
 const getNotes = require('./notes.js')
 const msg = getNotes()
 console.log(msg)
@@ -6,8 +8,12 @@ console.log(msg)
 
 console.log(validator.isEmail('11@qq.com'))
 console.log(validator.isURL('https://qq.com'))
-console.log(validator.isURL('https:/.com'))
+// console.log(validator.isURL('https:/.com'))
 
+
+console.log(chalk.bold.green.inverse('Success!'));
+console.log(chalk.red.bold('fail!'));
+console.log(chalk.bold.blue.underline.inverse('yeah'));
 
 
 
