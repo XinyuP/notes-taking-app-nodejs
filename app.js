bb26abf265ff7dc2d78694d1b334e12c952ec053
XinyuP <xinyup@umich.edu>
2022-12-12 19:44:40 -0500

    appendFileSync

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const fs = require('fs')
+const fs = require('node:fs')
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+//fs.writeFileSync('notes.txt', 'My name is Blaire!!!') // overwritten 
+fs.appendFileSync('notes.txt', ' My name is Blaire!!!') // append at the end  
